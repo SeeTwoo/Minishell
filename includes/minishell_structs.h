cce33d85d50ea7a6a163ae88a0ccac98cf86a61f
SeeTwoo <walti.bee@gmail.com>
2025-03-24 23:17:45 +0100

    saving

--- includes/minishell_structs.h
+++ includes/minishell_structs.h
@@ -6,32 +6,37 @@
 /*   By: walter <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 16:51:37 by walter            #+#    #+#             */
-/*   Updated: 2025/03/23 17:47:26 by walter           ###   ########.fr       */
+/*   Updated: 2025/03/24 20:09:25 by walter           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_STRUCTS_H
 # define MINISHELL_STRUCTS_H
 
-typedef struct s_list		t_list;
 typedef struct s_token		t_token;
 typedef struct s_scanner	t_scanner;
-typedef struct s_types		t_types
-
-struct s_list
-{
-	void	*content;
-	t_list	*next;
-}
 
 struct s_token
 {
 	char	*start;
-	size_t	size;;
+	size_t	size;
+	int		type;
 }
 
 struct s_scanner
 {
 	char	*start;
 	char	*current;
+}
+
+enum
+{
+	CMD,
+	REDIR,
+	HD,
+	PIPE,
+	OPEN_PAR,
+	CLOS_PAR,
+	AND,
+	OR
 }
--- src/lexer.c
+++ src/lexer.c
@@ -6,30 +6,35 @@
 /*   By: walter <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 18:18:22 by walter            #+#    #+#             */
-/*   Updated: 2025/03/23 19:58:17 by walter           ###   ########.fr       */
+/*   Updated: 2025/03/24 23:17:04 by walter           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_token	scan_token(char **line)
+t_token	*scan_token(t_scanner *scanner)
 {
-	
+	t_token	token;
+
+	scanner->current += ft_strspn(scanner->current, " \t\n");
+	if (!(*(scanner->current)))
+		return (NULL);
+	token.start = scanner->current;
+	scanner->current += ft_strcspn(scanner->current, " \t\n");
 }
 
 t_token	*lexer(char *line)
 {
-	t_token	*head;
-	t_token	*tail;
+	t_token		*head;
+	t_token		*tail;
+	t_scanner	scanner;
 
 	head = NULL;
 	tail = NULL;
-	while (*line)
+	scanner.current = line;
+	while (scanner.current)
 	{
-		s += ft_strchr(line, WHITE_SPACES);
-		tail = scan_token(&line);
-		if (!tail)
-			return (free_tokens);
+		tail = scan_token(&scanner);
 		if (!head)
 			head = tail;
 	}

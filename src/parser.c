e5b6c7e6e45910ef8e057c9acf5a616124ce3345
SeeTwoo <walti.bee@gmail.com>
2025-04-05 00:12:15 +0200

    added mods

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: walter <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 17:02:00 by walter            #+#    #+#             */
-/*   Updated: 2025/04/04 23:22:05 by walter           ###   ########.fr       */
+/*   Updated: 2025/04/05 00:11:59 by walter           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,11 @@ t_token	*find_lowest_right(t_token **tokens, int index)
 {
 	int	lowest;
 
-
+	lowest = tokens[index]->precedence
+	while (tokens[++index])
+		if (tokens[index]->precedence < lowest)
+			lowest = tokens[index]->precedence;
+	return (tokens[index]);
 }
 
 t_ast_node	*init_parsing(t_token **tokens)
@@ -24,6 +28,7 @@ t_ast_node	*init_parsing(t_token **tokens)
 	t_token	*lowest_precedence;
 
 	lowest_precedence = find_lowest_right(tokens, 0);
+	if (lowest->prece
 }
 
 t_ast_node	*parser(t_token **tokens)

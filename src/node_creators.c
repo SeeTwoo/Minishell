67e11d683626b6ac3eb45ebcce045b9532c34a30
SeeTwoo <walti.bee@gmail.com>
2025-04-05 18:54:56 +0200

    saving

---
+++ src/node_creators.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   node_creators.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: walter <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/05 17:31:36 by walter            #+#    #+#             */
+/*   Updated: 2025/04/05 18:54:46 by walter           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_ast_node	*redir_creator(t_token **tokens, int index)
+{
+	
+}
+
+t_ast_node	*cmd_creator(t_token **tokens, int index)
+{
+	
+}
+
+t_ast_node	pipe_creator(t_token **tokens, int index)
+{
+	
+}
+
+t_ast_node	logic_creator(t_token **tokens, int index)
+{
+	
+}
--- src/parser.c
+++ src/parser.c
@@ -6,13 +6,29 @@
 /*   By: walter <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 17:02:00 by walter            #+#    #+#             */
-/*   Updated: 2025/04/05 00:11:59 by walter           ###   ########.fr       */
+/*   Updated: 2025/04/05 17:31:18 by walter           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_token	*find_lowest_right(t_token **tokens, int index)
+t_ast_node	*new_ast_node(int type;)
+{
+	t_ast_node	*new;
+
+	new = malloc(sizeof(t_ast_node));
+	if (!new)
+		return (NULL);
+	new->left = NULL;
+	new->right = NULL;
+	new->visited = 0;
+	new->type = type;
+	new->args = NULL;
+	new->redirect = NULL;
+	return (new);
+}
+
+int	find_lowest_right(t_token **tokens, int index)
 {
 	int	lowest;
 
@@ -20,15 +36,18 @@ t_token	*find_lowest_right(t_token **tokens, int index)
 	while (tokens[++index])
 		if (tokens[index]->precedence < lowest)
 			lowest = tokens[index]->precedence;
-	return (tokens[index]);
+	return (tokens[lowest]);
 }
 
 t_ast_node	*init_parsing(t_token **tokens)
 {
-	t_token	*lowest_precedence;
+	int lowest;
 
-	lowest_precedence = find_lowest_right(tokens, 0);
-	if (lowest->prece
+	lowest = find_lowest_light(tokens, 0);
+	if (lowest->precedence = 2)
+		return (cmd_creator(lowest_precedence));
+	else
+		return (NULL);
 }
 
 t_ast_node	*parser(t_token **tokens)

9f564c5f4af0ea39a2261d5f68e0ef14d442afc8
SeeTwoo <walti.bee@gmail.com>
2025-04-22 14:07:42 +0200

    saving left parser

--- src/left_parser.c
+++ src/left_parser.c
@@ -12,6 +12,31 @@
 
 #include "minishell.h"
 
+int	find_lowest_left(t_token **tok, int i, int lim)
+{
+	int	lowest;
+	int	first;
+	int	till_open;
+
+	first = i;
+	lowest = i;
+	while (i >= 0 && tok[i]->prec > lim)
+	{
+		if (is_close_paren(tok[i]->type))
+		{
+			till_open = skip_paren_left(&tok[i - 1]);
+			if (i - till_open == 0)
+				return (find_lowest_left(tok, i - till_open + 1, lim));
+			else
+				return (find_lowest_left(tok, i - till_open - 1, lim));
+		}
+		if (tok[i]->prec <= tok[lowest]->prec)
+			lowest = i;
+		i++;
+	}
+	return (lowest);
+}
+
 t_ast_node	*parse_left(t_token **tok, int i, int lim)
 {
 	i = find_lowest_left(tok, i, lim);
